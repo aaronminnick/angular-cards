8302f9870af72130df1f2e97f25f152d310b1f4b
Aaron Minnick <abminnick@gmail.com>
2022-06-23 11:39:23 -0700

    add card component and property bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+  posts = [
+    { 
+      title: 'Neat Tree',
+      imageUrl: 'assets/tree.jpeg',
+      username: 'nature',
+      content: 'I saw this neat tree today.'
+    },
+    { 
+      title: 'Mountain',
+      imageUrl: 'assets/mountain.jpeg',
+      username: 'mountainlover',
+      content: 'A snowy mountain.'
+    },
+    { 
+      title: 'Mountain Biking',
+      imageUrl: 'assets/biking.jpeg',
+      username: 'biker69',
+      content: 'Bike till you (don\'t) like.'
+    }
+  ];
 }
